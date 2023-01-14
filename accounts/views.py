a97c593dbf891e38f47253953fddefeacf84d378
sagoresarker <sagoresarker.cse@gmail.com>
2023-01-15 01:02:30 +0600

    implement field and non-field error

--- accounts/views.py
+++ accounts/views.py
@@ -10,10 +10,39 @@ def registerUser(request):
         form = UserForm(request.POST)
 
         if form.is_valid():
+            '''
+            # Method 1
+            # Create the user using the form
+
+            # get the password from form
+            password = form.cleaned_data['password']
             user = form.save(commit=False)
+            # hash the password using set_password() method
+            user.set_password(password)
             user.role = User.CUSTOMER
             user.save()
+            '''
+
+            # Method 2
+            # Create the user using create_user method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            # Now pass the value in the models.py file and use create_user method
+
+            user = User.objects.create_user(first_name=first_name, last_name=last_name,
+                                    username=username, email=email, password=password)
+            user.role = User.CUSTOMER
+            user.save()
+
             return redirect('registerUser')
+        else:
+            print('Invalid user')
+
+
     else:
         form = UserForm()
     context = {
