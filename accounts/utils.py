067ec99b358d21b27449e032268ce57be2a2a3ea
sagoresarker <sagoresarker.cse@gmail.com>
2023-01-18 01:28:47 +0600

    Add Admin Aproval email functionality for Vendor

--- accounts/utils.py
+++ accounts/utils.py
@@ -55,3 +55,11 @@ def send_password_reset_email(request, user, mail_subject, email_template):
 
 
 """
+
+def send_notification(mail_subject, mail_template, context):
+    from_email = settings.DEFAULT_FROM_EMAIL
+    message = render_to_string(mail_template, context)
+    to_email = context['user'].email
+    mail = EmailMessage(mail_subject, message, from_email, to=[to_email])
+    mail.send()
+    return
